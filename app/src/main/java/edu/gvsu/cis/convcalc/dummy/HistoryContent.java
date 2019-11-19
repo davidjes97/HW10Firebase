5c3fb7caeb325c5d16c415b93d9a13c2c5462384
davidjes97 <davidjes@mail.gvsu.edu>
2019-11-18 23:58:58 -0500

    All the stuff
    Bascially the third time ive tried setting this up in git

---
+++ app/src/main/java/edu/gvsu/cis/convcalc/dummy/HistoryContent.java
@@ -0,0 +1,50 @@
+package edu.gvsu.cis.convcalc.dummy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class HistoryContent {
+
+    public static final List<HistoryItem> ITEMS = new ArrayList<HistoryItem>();
+
+    public static void addItem(HistoryItem item) {
+        ITEMS.add(item);
+    }
+
+    public static class HistoryItem {
+        public Double fromVal;
+        public Double toVal;
+        public String mode;
+        public String fromUnits;
+        public String toUnits;
+
+        public String timestamp;
+        public String _key;
+
+        public HistoryItem() {
+
+        }
+
+        public HistoryItem(Double fromVal, Double toVal, String mode,
+                           String fromUnits, String toUnits, String timestamp) {
+            this.fromVal = fromVal;
+            this.toVal = toVal;
+            this.mode = mode;
+            this.fromUnits = fromUnits;
+            this.toUnits = toUnits;
+            this.timestamp = timestamp;
+        }
+
+        @Override
+        public String toString() {
+            return this.fromVal + " " + this.fromUnits + " = " + this.toVal + " " + this.toUnits;
+        }
+    }
+}
+
